fc2d1f15e3049242a28653360cb10f23516bd0ee
TheTallPaul <paul.lantow@gmail.com>
2020-06-10 10:31:00 -0700

    Fixed spacing NaN bug

--- src/app/add-event/add-event.component.ts
+++ src/app/add-event/add-event.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, NgForm } from '@angular/forms';
 import { LatLngBoundsLiteral } from '@agm/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import * as firebase from 'firebase/app';
 
@@ -15,7 +16,7 @@ export class AddEventComponent implements OnInit {
 
   lat = 45.509219;
   lng = -122.5394805;
-  feetToMeters: 0.3048;
+  feetToMeters = 0.3048;
 
   // Mill End Park
   north = 45.509668;
@@ -23,7 +24,11 @@ export class AddEventComponent implements OnInit {
   south = 45.50877;
   west = -122.540251;
 
-  constructor(public eventsService: EventsService) {}
+  constructor(
+    public eventsService: EventsService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
 
@@ -35,14 +40,18 @@ export class AddEventComponent implements OnInit {
   }
 
   onSubmit() {
-    console.warn(this.eventsService.form.value);
     const event = this.eventsService.form.value;
+
+    // Build boundary points
     event.nw_boundary = new firebase.firestore.GeoPoint(this.north, this.west);
     event.se_boundary = new firebase.firestore.GeoPoint(this.south, this.east);
+
     // Convert to meters
-    event.spacing_meters = event.spacing_meters * this.feetToMeters;
+    event.spacing_meters *= this.feetToMeters;
 
     this.eventsService.createEvent(event)
-      .then(res => {console.log(event); });
+      .then(res => {
+        this.router.navigate(['/view-event/' + res.id]);
+      });
   }
 }
